208acc736870f618f33ceb9db9f66269c9d50e24
timwlee <timothyl@adobe.com>
2025-02-19 15:14:35 -0800

    Update teaser block to be able to wrap the content of the block in an anchor tag based off provided url

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,14 +1,36 @@
 export default function decorate(block) {
-  // setup image teaser
+  const wrapTeaserWithLink = block.classList.contains('teaser-link-true');
+  const link = block.querySelector('a');
+
   [...block.children].forEach((row) => {
-    const pic = row.querySelector('picture');
-    if (pic) {
-      const picWrapper = pic.parentElement;
-      if (picWrapper && picWrapper.children.length === 1) {
-        // picture is only content in column
-        picWrapper.classList.add('teaser-img-col');
-      }
+    setupImageTeaser(row);
+
+    if (wrapTeaserWithLink && link) {
+      wrapRowWithLink(row, link.href);
+      link.remove();
+    } else {
+      row.classList.add('wrapper');
     }
-    row.classList.add('wrapper');
   });
+}
+
+function setupImageTeaser(row) {
+  const pic = row.querySelector('picture');
+  if (pic) {
+    const picWrapper = pic.parentElement;
+    if (picWrapper && picWrapper.children.length === 1) {
+      picWrapper.classList.add('teaser-img-col');
+    }
+  }
+}
+
+function wrapRowWithLink(row, href) {
+  const anchor = document.createElement('a');
+  anchor.href = href;
+  anchor.classList.add('link-wrapper');
+
+  while (row.firstChild) {
+    anchor.appendChild(row.firstChild);
+  }
+  row.replaceWith(anchor);
 }
